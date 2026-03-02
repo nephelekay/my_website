8a539d2923f9bcd626eac8d8a894576a6250a3e4
nephelekay <nephelekay1@gmail.com>
2026-03-01 23:08:22 -0600

    Welcome message

---
+++ app/components/welcome.tsx
@@ -0,0 +1,18 @@
+export default function Welcome() {
+  return (
+    <div>
+      <h1 className="text-blue-200 font-bold text-4xl">
+        Welcome to my website!
+      </h1>
+      <p className="text-gray-600 font-sans mt-2">
+        My name is Nephele Kay! I am a Computer Science student in Chicago, Illinois.
+      </p>
+      <img
+        src="/mist_sky.jpg"
+        alt="Mist Sky"
+        width="400"
+        className="block mx-auto mt-4"
+      />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import Welcome from "./components/welcome";
 
 export default function Home() {
   return (
+    <div>
     <div className="flex flex-row gap-4">
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
       <Link href="/projects">Projects</Link>
     </div>
+
+      <Welcome />
+    </div>
+    
   );
 }
